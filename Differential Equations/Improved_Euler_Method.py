a0d9e7ca2381da7a1c0488399d52d6d9a216568b
Mixuan Pan <panmixuan@outlook.com>
2025-02-16 11:48:37 -0500

    clearer documentation & explanation

--- Differential Equations/Improved_Euler_Method.py
+++ Differential Equations/Improved_Euler_Method.py
@@ -9,14 +9,19 @@ Start with step size h = 0.1, and then successively smaller step sizes until suc
 import math 
 import numpy as np
 
-y = 0
-x = 0
-h = [0.1, 0.01, 0.001, 0.0001]
+y = 0 # starting value for y 
+x = 0 # starting value for x; using the interval [0, 2] in this example 
+h = [0.1, 0.01, 0.001, 0.0001] # step sizes 
 
 for i in h:
-    for j in np.arange(0, 2, i):
-        y += i * (pow(x, 2) + 3 * pow(y, 2) - 7)
+    for j in np.arange(0, 2, i): # use np.arange to easily loop with decimal increments 
+        # dy / dx is given in this example 
+        y += i * (pow(x, 2) + 3 * pow(y, 2) - 7) # (step size) * (dy / dx) 
         x += i
     print("The approximation when h = ", i, " is ", y)
-    x = 0
+    # reset the values of x and y for the next step size 
+    x = 0 
     y = 0
+
+# compare the result of each step for the decimal places 
+# as long as the function is not periodic, the approximation with the less step size should be more accurate, given the more number of accurate decimal places
--- Series/unrepeated.py
+++ Series/unrepeated.py
@@ -1,11 +1,12 @@
 # find the number of unrepeated pairs 
 
-
+# example list 
 elements = [1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 6, 6, 6, 6] 
-n = len(elements)
+n = len(elements) # length of elements 
 s = 0 # sum number 
 for i in range(0, n-1):
     s += bool(elements[i+1] - elements[i])
+    
     # the term is repeated if the difference between the current term 
     # and the next term is 0 
 
@@ -13,4 +14,16 @@ for i in range(0, n-1):
     # bool makes the difference equal to 1 when the difference is 
     # greater than 1
 
-print("number of unrepeated pairs: ", s * (s + 1) / 2)
+s = s * (s + 1) / 2
+
+# the first 1 should be counted once, the second 1 should be counted twice
+# in this example: 
+# [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0]
+# _____________________________________________________  # first addition  
+#           ___________________________________________  # second addition
+#                       _______________________________  # third addition 
+#                                ______________________  # fourth addition 
+#                                      ________________  # fifth addition 
+# sum = 5 + 4 + 3 + 1 = 5 * (5 - 1) / 2
+
+print("number of unrepeated pairs: ", s)
